ed80971b5cc7b19a53a990d5ac774ca1f8a10f61
Helias <stefanoborzi32@gmail.com>
2015-10-02 17:28:36 +0200

    Implemented algorithm to recognize the AllowableRaces, added races picture

--- js/app/main.js
+++ js/app/main.js
@@ -5,19 +5,37 @@
   'use strict';
   var app = angular.module('questbugged', ['ui.bootstrap', 'tableSort']);
 
-  app.controller("QuestBugged", function($scope, $http) {
+  app.controller("QuestBugged", function($scope, $http, $sce) {
 
     $scope.quests = [];
-    
+
     $http.get( app.api + "quest/bugged/" )
       .success(function(data, status, header, config) {
-      $scope.quests = data;
       for (var i = 0; i < data.length; i++)
       {
-        if (data[i].AllowableRaces == "609")
-          
+        if (data[i].AllowableRaces == "0")
+          data[i].AllowableRaces = $sce.trustAsHtml('<img src="img/alliance.png"><img src="img/horde.png">');
+        else if (data[i].AllowableRaces == "690")
+          data[i].AllowableRaces = $sce.trustAsHtml('<img src="img/horde.png">');
+        else if (data[i].AllowableRaces == "1101")
+          data[i].AllowableRaces = $sce.trustAsHtml('<img src="img/alliance.png">');
+        else
+        {
+          var allowableRace = "", race = 0;
+
+          data[i].AllowableRaces = String(parseInt(data[i].AllowableRaces, 10).toString(2));
+          data[i].AllowableRaces = data[i].AllowableRaces.split("").reverse().join("");
+
+          for (var j = 0; j < String(data[i].AllowableRaces).length; j++)
+          {
+            race = parseInt(data[i].AllowableRaces[j], 10);
+            if (race == 1)
+              allowableRace += '<img src="img/'+(j+1)+'.gif">';
+          }
+          data[i].AllowableRaces = $sce.trustAsHtml(allowableRace);
+        }
       }
-      //690 horde  1101 Alliance
+      $scope.quests = data;
     })
       .error(function(data, status, header, config) {
       console.log("Error in QUEST $http.get");
@@ -26,3 +44,4 @@
   });
 
 }());
+
